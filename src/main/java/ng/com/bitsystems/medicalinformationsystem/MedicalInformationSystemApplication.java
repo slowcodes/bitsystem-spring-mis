807e6fde4b58bae1bbc78a7b6c0e4e592ed637b9
Ezenna Kelechi <charlesezenna@gmail.com>
2020-07-27 23:10:42 +0100

    initial commit

---
+++ src/main/java/ng/com/bitsystems/medicalinformationsystem/MedicalInformationSystemApplication.java
@@ -0,0 +1,13 @@
+package ng.com.bitsystems.medicalinformationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalInformationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalInformationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ng/com/bitsystems/medicalinformationsystem/MedicalInformationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ng.com.bitsystems.medicalinformationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalInformationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
