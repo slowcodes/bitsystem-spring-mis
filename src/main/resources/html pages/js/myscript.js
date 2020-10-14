36db2cb8733fb0003aac3cf333f1e52c76eaf798
kezenna <kezenna@bitbucket.org>
2020-10-14 13:00:44 +0100

    html page setup10/14

---
+++ src/main/resources/html pages/js/myscript.js
@@ -0,0 +1,10 @@
+  $( function() {
+	 $('#table_id').DataTable();
+	
+	});
+	
+    $( "#datepicker" ).datepicker();
+    $( "#anim" ).on( "change", function() {
+      $( "#datepicker" ).datepicker( "option", "showAnim", $( this ).val() );
+    });
+  } );
